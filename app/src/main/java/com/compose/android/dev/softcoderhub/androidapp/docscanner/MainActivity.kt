7afec5fedff84a7b9841b3c663726f92033baf6d
Devajit <patar1447@gmail.com>
2024-03-08 22:32:56 +0530

    App Tool Bar  and Primary Color Update

--- app/src/main/java/com/compose/android/dev/softcoderhub/androidapp/docscanner/MainActivity.kt
+++ app/src/main/java/com/compose/android/dev/softcoderhub/androidapp/docscanner/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        Thread.sleep(3000)
+
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
